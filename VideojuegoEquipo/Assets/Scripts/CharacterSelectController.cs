24a09f024514683d3ca42bc70599639e746fe3eb
Anye1990 <angiemenchaca20@gmail.com>
2025-11-25 16:59:53 -0700

    personajes estructura

--- VideojuegoEquipo/Assets/Scripts/CharacterSelectController.cs
+++ VideojuegoEquipo/Assets/Scripts/CharacterSelectController.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class CharacterSelectController : MonoBehaviour
 {
+    public AnimatorOverrideController[] characterControllers; 
+    void Start()
+    {
+        int index = PlayerPrefs.GetInt("SelectedCharacter");
+        GetComponent<Animator>().runtimeAnimatorController = characterControllers[index];
+    }
     public void SelectCharacter(int characterIndex)
     {
         PlayerPrefs.SetInt("SelectedCharacter", characterIndex);
--- VideojuegoEquipo/Assets/Scripts/PlayerController.cs
+++ VideojuegoEquipo/Assets/Scripts/PlayerController.cs
@@ -5,22 +5,49 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 8f;
     private Rigidbody2D rb;
+    private Animator animator;
+
+    public RuntimeAnimatorController[] characterAnimators;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        // Aquí puedes cargar el personaje seleccionado si tienes diferentes sprites
+        animator = GetComponent<Animator>();
+
+        // Lee qué personaje se eligió (0, 1 o 2)
+        int selectedCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+
+        // Asigna el controlador correcto
+        if (characterAnimators.Length > 0 && selectedCharacterIndex < characterAnimators.Length)
+        {
+            animator.runtimeAnimatorController = characterAnimators[selectedCharacterIndex];
+        }
     }
 
     private void Update()
     {
         float move = Input.GetAxis("Horizontal");
+
+        // Recuerda usar rb.linearVelocity si estás en Unity 6, o rb.velocity en versiones anteriores
         rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        if (move > 0) transform.localScale = new Vector3(1, 1, 1);
+        else if (move < 0) transform.localScale = new Vector3(-1, 1, 1);
+
+        animator.SetFloat("Speed", Mathf.Abs(move));
 
         if (Input.GetButtonDown("Jump"))
         {
-            // Chequea si está en el piso antes de saltar
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            if (Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                animator.SetBool("IsJumping", true);
+            }
+        }
+
+        if (Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+        {
+            animator.SetBool("IsJumping", false);
         }
     }
 
