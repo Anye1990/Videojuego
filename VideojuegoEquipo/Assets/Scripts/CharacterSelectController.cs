21e76611de344727ffc6895d97b621200dfc129b
Anye1990 <angiemenchaca20@gmail.com>
2025-11-27 19:57:34 -0700

    YA ME CANSE

--- VideojuegoEquipo/Assets/Scripts/CharacterSelectController.cs
+++ VideojuegoEquipo/Assets/Scripts/CharacterSelectController.cs
@@ -3,16 +3,56 @@ using UnityEngine.SceneManagement;
 
 public class CharacterSelectController : MonoBehaviour
 {
-    public AnimatorOverrideController[] characterControllers; 
+    [Header("Configuración")]
+    // Arrastra aquí al personaje único que está en el podio
+    public Animator displayCharacterAnimator;
+
+    // Arrastra aquí los MISMOS controladores que pusiste en tu PlayerController
+    // (Beige, Green, Pink, etc.)
+    public RuntimeAnimatorController[] characterData;
+
+    private int selectedIndex = 0;
+
     void Start()
     {
-        int index = PlayerPrefs.GetInt("SelectedCharacter");
-        GetComponent<Animator>().runtimeAnimatorController = characterControllers[index];
-    }
-    public void SelectCharacter(int characterIndex)
-    {
-        PlayerPrefs.SetInt("SelectedCharacter", characterIndex);
-        SceneManager.LoadScene("Level1");
+        // Recuperar la última elección o empezar en 0
+        selectedIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+        UpdateCharacterShowcase();
     }
 
+    public void NextCharacter()
+    {
+        selectedIndex++;
+        if (selectedIndex >= characterData.Length)
+        {
+            selectedIndex = 0;
+        }
+        UpdateCharacterShowcase();
+    }
+
+    public void PreviousCharacter()
+    {
+        selectedIndex--;
+        if (selectedIndex < 0)
+        {
+            selectedIndex = characterData.Length - 1;
+        }
+        UpdateCharacterShowcase();
+    }
+
+    void UpdateCharacterShowcase()
+    {
+        // Aquí ocurre la magia: cambiamos el "cerebro" de animación del maniquí
+        if (characterData.Length > 0 && displayCharacterAnimator != null)
+        {
+            displayCharacterAnimator.runtimeAnimatorController = characterData[selectedIndex];
+        }
+    }
+
+    public void PlayGame()
+    {
+        PlayerPrefs.SetInt("SelectedCharacter", selectedIndex);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("Level1");
+    }
 }
